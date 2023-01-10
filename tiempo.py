e40b1e23c05d98d93c057807ebdc39e05921c0be
gomolajo <114004031+gomolajo@users.noreply.github.com>
2023-01-10 16:18:05 +0100

    Add files via upload

---
+++ tiempo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def tierra(param):
+    valor=param**2*9.8/2
+    return (valor)
+
+def marte(param):
+    valor=param**2*3.7/2
+    return (valor)
+
+def jupiter(param):
+    valor=param**2*24.8/2
+    return (valor)
+
